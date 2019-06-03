8c3d8a5c8ca0e3da8450675115f35dff1af4f5ab
barton-w <bartonw@gmail.com>
2019-06-02 20:43:08 -0600

    User authentication is working

--- controllers/account.js
+++ controllers/account.js
@@ -8,14 +8,27 @@ const errorResponse = "Oh no, an error occurred! Please try again";
 //////////////////
 ////GET routes////
 //////////////////
-router.get("/new", (request, response) => {
-  response.render("account/new.ejs");
+router.get("/create", (request, response) => {
+  if (request.session.currentUser) {
+    response.redirect("/");
+  } else {
+    response.render("account/create.ejs");
+  };
+});
+router.get("/login", (request, response) => {
+  if (request.session.currentUser) {
+    response.redirect("/");
+  } else {
+    response.render("account/login.ejs", {
+      message: "none"
+    });
+  };
 });
 
 //////////////////
 ////POST routes///
 //////////////////
-router.post("/", (request, response) => {
+router.post("/create", (request, response) => {
   request.body.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10));
   User.create(request.body, (error, createdUser) => {
     if (error) {
@@ -25,6 +38,27 @@ router.post("/", (request, response) => {
       request.session.currentUserName = createdUser.username;
       request.session.currentUser = createdUser._id;
       response.redirect("/");
+    };
+  });
+});
+router.post("/login", (request, response) => {
+  console.log(request.body);
+  User.findOne({username: request.body.username}, (error, foundUser) => {
+    if (error) {
+      console.log(error);
+      response.send(errorResponse);
+    } else if (!foundUser) {
+      response.render("account/login.ejs", {
+        message: "We didn't find that username, please try again"
+      });
+    } else if (bcrypt.compareSync(request.body.password, foundUser.password)) {
+      request.session.currentUserName = foundUser.username;
+      request.session.currentUser = foundUser._id;
+      response.redirect("/");
+    } else {
+      response.render("account/login.ejs", {
+        message: "Incorrect password, please try again"
+      });
     };
   });
 });
