a197ea077fe87ee52d36afbf7917b3d3a29eb1b0
barton-w <bartonw@gmail.com>
2019-06-03 23:10:34 -0600

    User is able to edit posts, and editing permissions are controlled by session parameters

--- controllers/trails.js
+++ controllers/trails.js
@@ -6,6 +6,7 @@ const errorResponse = "Oh no, an error occurred! Please try again";
 //////////////////
 ////GET routes////
 //////////////////
+//Main page
 router.get("/", (request, response) => {
   console.log(request.session.currentUser);
   console.log(request.session.currentUserName);
@@ -29,6 +30,7 @@ router.get("/", (request, response) => {
     };
   }).sort({createdAt: -1}).limit(10);
 });
+//Route to a form to create a post
 router.get("/add", (request, response) => {
   console.log(request.session.currentUser);
   if (request.session.currentUser) {
@@ -37,8 +39,52 @@ router.get("/add", (request, response) => {
     response.redirect("/account/login");
   };
 });
+//Show route for individual posts
 router.get("/view/:id", (request, response) => {
-  response.send("VIEW A TRIAL");
+  let currentUser = "none";
+  let currentUserName = "none";
+  if (request.session.currentUser) {
+    currentUser = request.session.currentUser;
+    currentUserName = request.session.currentUserName;
+  };
+  Trail.find({_id: request.params.id}, (error, foundTrail) => {
+    console.log(foundTrail[0].share);
+    console.log(foundTrail[0].user);
+    console.log(currentUser);
+    if (error) {
+      console.log(error);
+      response.send(errorResponse);
+    //Restricting access to un-shared posts when they don't belong to the logged-in user
+    } else if (foundTrail[0].share === false && foundTrail[0].user !== currentUser) {
+      response.redirect("/trails");
+    } else {
+      response.render("trails/show.ejs", {
+        trail: foundTrail,
+        currentUser: currentUser,
+        currentUserName: currentUserName
+      });
+    };
+  });
+});
+//Route to a form to edit an existing post
+router.get("/edit/:id", (request, response) => {
+  //If the user isn't logged in, they can't edit anything
+  if (!request.session.currentUser) {
+    response.redirect("/trails");
+  } else {
+    Trail.find({_id: request.params.id}, (error, foundTrail) => {
+      if (error) {
+        console.log(error);
+        response.send(errorResponse);
+      } else if (foundTrail[0].user !== request.session.currentUser) {
+        response.redirect("/trails");
+      } else {
+        response.render("trails/edit.ejs", {
+          trail: foundTrail
+        });
+      };
+    });
+  };
 });
 
 //////////////////
@@ -53,6 +99,21 @@ router.post("/add", (request, response) => {
       response.send(errorResponse);
     } else {
       response.redirect("/");
+    };
+  });
+});
+
+//////////////////
+////PUT routes////
+//////////////////
+router.put("/edit/:id", (request, response) => {
+  request.body.share === "on" ? request.body.share = true : request.body.share = false;
+  Trail.updateOne({_id: request.params.id}, request.body, (error, updatedTrail) => {
+    if (error) {
+      console.log(error);
+      response.send(errorResponse);
+    } else {
+      response.redirect("/trails");
     };
   });
 });
